b4170f0804d1356ed9b0dac8b0bd00ed4ba66ab6
Juan Ignacio Avendaño Huego <juanihu94@gmail.com>
2017-03-01 23:01:07 +0100

    Getting Data from database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,36 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
+using System.Data.Entity;
 using System.Web.Mvc;
 using Vidly.Models;
-using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            return View(new LayoutViewModel());
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            LayoutViewModel viewModel = new LayoutViewModel();
-            viewModel.CustomerSelected = id;
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-            //We Shall check if customer really exists...
-            foreach (Customer customer in viewModel.Customers)
-            {
-                if ( id == customer.Id )
-                {
-                    return View(customer);
-                }
-            }
+            if ( customer == null)
+                return HttpNotFound();
 
-            return HttpNotFound();
+            return View(customer);
             
         }
     }
